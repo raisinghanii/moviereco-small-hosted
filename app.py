1640d6097d6b56939505e064147ba730dc2098fd
raisinghanii <46864533+raisinghanii@users.noreply.github.com>
2020-03-08 18:32:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+app = Flask(__name__)
 
 def get_title_from_index(index):
     return df[df.index == index]["title"].values[0]
