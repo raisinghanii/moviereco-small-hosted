6c24acd1ad625af321c891c4f461a8900b4d957a
raisinghanii <46864533+raisinghanii@users.noreply.github.com>
2020-03-08 22:19:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from fuzzywuzzy import fuzz
 
 def get_title_from_index(df,index):
     return df[df.index == index]["title"].values[0]
@@ -40,32 +39,12 @@ def calcsim():
 
 
 
-# def fuzzy_matching(mapper, fav_movie, verbose=True):
-
-#     match_tuple = []
-
-#     for i in mapper:
-#         ratio = fuzz.ratio(i.lower(),fav_movie.lower())
-#         if ratio>=60:
-#             match_tuple.append((i,ratio))
-
-#     match_tuple = sorted(match_tuple, key=lambda x: x[1])[::-1]
-#     if not match_tuple:
-#         return "00"
-#     if verbose:
-#         return match_tuple[0][0]
-
-
 def suggest(movie_user_likes):
     try:
         df.head()
         cosine_sim.shape
     except:
         df,cosine_sim = calcsim()
-
-
-#     movie_user_like = fuzzy_matching(df['title'],movie_user_likes)
-#     print("movie likes = ",movie_user_like)
 
     movie_index = get_index_from_title(df,movie_user_likes)
 
@@ -77,14 +56,12 @@ def suggest(movie_user_likes):
 
     i = 0
     for element in sorted_similar_movies:
-        # print(get_title_from_index(element[0]))
         movies.append(get_title_from_index(df,element[0]))
         i = i + 1
         if i > 50:
             break
 
     return movies
-
 
 
 app = Flask(__name__)
@@ -97,13 +74,9 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
     int_features = [str(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    print(*final_features[0])
-
+    
     try:
         list = []
         res = suggest(*final_features[0])
