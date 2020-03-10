c629a5de596becd4ac7896a7983268baba0c91a8
raisinghanii <46864533+raisinghanii@users.noreply.github.com>
2020-03-10 14:48:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from fuzzywuzzy import fuzz
 
 def get_title_from_index(df,index):
     return df[df.index == index]["title"].values[0]
@@ -39,6 +40,22 @@ def calcsim():
 
 
 
+def fuzzy_matching(mapper, fav_movie, verbose=True):
+
+    match_tuple = []
+
+    for i in mapper:
+        ratio = fuzz.ratio(i.lower(),fav_movie.lower())
+        if ratio>=60:
+            match_tuple.append((i,ratio))
+
+    match_tuple = sorted(match_tuple, key=lambda x: x[1])[::-1]
+    if not match_tuple:
+        return "00"
+    if verbose:
+        return match_tuple[0][0]
+
+
 def suggest(movie_user_likes):
     try:
         df.head()
@@ -46,7 +63,11 @@ def suggest(movie_user_likes):
     except:
         df,cosine_sim = calcsim()
 
-    movie_index = get_index_from_title(df,movie_user_likes)
+
+    movie_user_like = fuzzy_matching(df['title'],movie_user_likes)
+    print("movie likes = ",movie_user_like)
+
+    movie_index = get_index_from_title(df,movie_user_like)
 
     similar_movies = list(enumerate(cosine_sim[movie_index]))
 
@@ -56,12 +77,14 @@ def suggest(movie_user_likes):
 
     i = 0
     for element in sorted_similar_movies:
+        # print(get_title_from_index(element[0]))
         movies.append(get_title_from_index(df,element[0]))
         i = i + 1
         if i > 50:
             break
 
     return movies
+
 
 
 app = Flask(__name__)
@@ -74,9 +97,13 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
     int_features = [str(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    
+    print(*final_features[0])
+
     try:
         list = []
         res = suggest(*final_features[0])
